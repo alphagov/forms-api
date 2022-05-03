518d46e8f227fc157e7ba325950a4d3098c2aa90
Tristram Oaten <tris@oat.sh>
2022-05-03 17:10:46 +0100

    Stub form api

--- lib/services/example.rb
+++ lib/services/example.rb
@@ -2,4 +2,16 @@ class Services::Example
   def execute(example)
     "GOV.UK Forms API: #{example}"
   end
+
+  def get_forms(name)
+
+    JSON({
+        name => {
+            item2: {
+                item3: 'ok'
+            }
+        }
+    })
+  end
+
 end
--- spec/services/example_spec.rb
+++ spec/services/example_spec.rb
@@ -1,5 +1,6 @@
 describe Services::Example do
   it "works" do
-    expect(described_class.new.execute("test")).to eq("GOV.UK Forms API: test")
+    expect(described_class.new.get_forms("test"))
+      .to eq("{\"test\":{\"item2\":{\"item3\":\"ok\"}}}")
   end
 end

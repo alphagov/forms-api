a75c3f3953d23c0f6b806e167d784190fa85d5c1
Tristram Oaten <tris@oat.sh>
2022-07-05 11:17:02 +0100

    Add org logic to repository
    Co-authored-by: Daniel Burnley <dan@danops.uk>
    Co-authored-by: alice-carr <alice-carr@users.noreply.github.com>

--- spec/repositories/forms_repository_spec.rb
+++ spec/repositories/forms_repository_spec.rb
@@ -5,36 +5,39 @@ describe Repositories::FormsRepository do
 
   context "creating a new form" do
     it "creates a form" do
-      result = subject.create("name", "submission_email")
+      result = subject.create("name", "submission_email", "org")
       created_form = database[:forms].where(id: result).all.last
       expect(created_form[:name]).to eq("name")
       expect(created_form[:submission_email]).to eq("submission_email")
+      expect(created_form[:org]).to eq("org")
     end
   end
 
   context "getting a form" do
     it "gets a form" do
-      form_id = subject.create("name", "submission_email")
+      form_id = subject.create("name", "submission_email", "org")
       form = subject.get(form_id)
       expect(form[:name]).to eq("name")
       expect(form[:submission_email]).to eq("submission_email")
+      expect(form[:org]).to eq("org")
     end
   end
 
   context "updating a form" do
     it "updates a form" do
-      form_id = subject.create("name", "submission_email")
-      update_result = subject.update(form_id, "name2", "submission_email2")
+      form_id = subject.create("name", "submission_email", "org")
+      update_result = subject.update(form_id, "name2", "submission_email2", "org2")
       form = subject.get(form_id)
       expect(update_result).to eq(1)
       expect(form[:name]).to eq("name2")
       expect(form[:submission_email]).to eq("submission_email2")
+      expect(form[:org]).to eq("org2")
     end
   end
 
   context "deleting a form" do
     it "deletes a form" do
-      form_id = subject.create("name", "submission_email")
+      form_id = subject.create("name", "submission_email", "org")
       result = subject.delete(form_id)
       expect(result).to eq(1)
     end
@@ -42,8 +45,8 @@ describe Repositories::FormsRepository do
 
   context "getting all forms" do
     it "gets all forms" do
-      form_id1 = subject.create("name", "submission_email")
-      form_id2 = subject.create("name", "submission_email")
+      form_id1 = subject.create("name", "submission_email", "org")
+      form_id2 = subject.create("name", "submission_email", "org")
       forms = subject.fetch_all
       expect(forms.length).to eq(2)
       expect(forms[0][:id]).to eq(form_id1)

ceaedbf46371b397924cabd74cfe49558482f52c
alice-carr <alice.bc.carr@gmail.com>
2022-07-25 16:11:07 +0100

    mock env vars

--- spec/lib/server_spec.rb
+++ spec/lib/server_spec.rb
@@ -7,22 +7,43 @@ describe Server::Server do
     Server::Server
   end
 
-  context "GET /api/v1/forms" do
+  context "when API_KEY set in ENV" do
+    before do
+      stub_const("ENV", ENV.to_hash.merge("API_KEY" => "an-api-key"))
+    end
+
     it "returns a status code 401 when no authentication supplied" do
-      get "/api/v1/forms"
-      expect(last_response.status).to eq(401) unless ENV["API_KEY"].nil?
+      get "/api/v1/forms", { org: "gds" }
+      expect(last_response.status).to eq(401)
     end
 
     it "returns a status code 401 when incorrect API key supplied" do
       header "X-Api-Token", "an-incorrect-api-token"
-      get "/api/v1/forms"
+      get "/api/v1/forms", { org: "gds" }
       expect(last_response.status).to eq(401)
     end
 
     it "returns a status code 200 when correct API key supplied" do
       header "X-Api-Token", ENV["API_KEY"]
-      get "/api/v1/forms"
-      expect(last_response.status).to eq(200) unless ENV["API_KEY"].nil?
+      get "/api/v1/forms", { org: "gds" }
+      expect(last_response.status).to eq(200)
+    end
+  end
+
+  context "when no API_KEY set in ENV" do
+    before do
+      stub_const("ENV", ENV.to_hash.merge("API_KEY" => nil))
+    end
+
+    it "returns a status code 200 when supplied with incorrect api key" do
+      header "X-Api-Token", "an-api-key"
+      get "/api/v1/forms", { org: "gds" }
+      expect(last_response.status).to eq(200)
+    end
+
+    it "returns a status code 200 when supplied with no api key" do
+      get "/api/v1/forms", { org: "gds" }
+      expect(last_response.status).to eq(200)
     end
   end
 end

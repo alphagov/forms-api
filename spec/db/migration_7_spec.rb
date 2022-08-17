c9e00cdc5d8d89f74d7e355859bbf001d92b8762
Tristram Oaten <tris@oat.sh>
2022-08-17 18:06:31 +0100

    rubocop ate my homework

--- spec/db/migration_7_spec.rb
+++ spec/db/migration_7_spec.rb
@@ -7,12 +7,11 @@ describe "migration 7" do
     migrator.migrate_to(database, 6)
   end
   it "Update old forms with a created and updated date" do
-    no_created_form = database[:forms].insert(name: "Form 2", submission_email: "submission_email")
-    uncreated_forms = database[:forms].where(created_at: nil)
+    database[:forms].where(created_at: nil)
 
     @database[:forms].where(created_at: nil).update(
       updated_at: Time.now,
-      created_at: Time.now,
+      created_at: Time.now
     )
 
     expect(@database[:forms].where(created_at: nil).first).to be_nil

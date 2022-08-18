3cc19282391e7c63c47aa06a8100bc7f58d3dbb9
Tristram Oaten <tris@oat.sh>
2022-08-18 09:13:19 +0100

    Update migration_7_spec.rb

--- spec/db/migration_7_spec.rb
+++ spec/db/migration_7_spec.rb
@@ -7,7 +7,6 @@ describe "migration 7" do
     migrator.migrate_to(database, 6)
   end
   it "Update old forms with a created and updated date" do
-    database[:forms].where(created_at: nil)
 
     @database[:forms].where(created_at: nil).update(
       updated_at: Time.now,

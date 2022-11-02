b325eeab4e7f30d77e6c0bae63e55932705abb77
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-11-02 20:49:25 +0000

    Gracefully handle nil form names being sent

--- lib/repositories/forms_repository.rb
+++ lib/repositories/forms_repository.rb
@@ -25,7 +25,7 @@ class Repositories::FormsRepository
       what_happens_next_text: form[:what_happens_next_text],
       declaration_text: form[:declaration_text],
       question_section_completed: form[:question_section_completed],
-      form_slug: form[:name].parameterize,
+      form_slug: form[:name]&.parameterize,
       support_email: form[:support_email],
       support_phone: form[:support_phone],
       support_url: form[:support_url],

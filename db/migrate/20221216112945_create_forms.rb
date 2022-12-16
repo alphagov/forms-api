752236cb1c4e98870277b6d05ca5f47e44a1b6e0
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-12-16 13:10:14 +0000

    revert setting form id type and default in the migration
    Co-authored-by: Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>

--- db/migrate/20221216112945_create_forms.rb
+++ db/migrate/20221216112945_create_forms.rb
@@ -1,6 +1,6 @@
 class CreateForms < ActiveRecord::Migration[7.0]
   def change
-    create_table :forms, id: :bigint, default: nil, if_not_exists: true do |t|
+    create_table :forms, if_not_exists: true do |t|
       t.text :name
       t.text :submission_email
       t.text :org

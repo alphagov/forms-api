b5b2d6fcc97fd7431920b6ac073e3759b1de10fd
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-12-16 12:51:25 +0000

    tweaks to forms factory
    Co-authored-by: Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>

--- spec/factories/forms.rb
+++ spec/factories/forms.rb
@@ -1,18 +1,48 @@
 FactoryBot.define do
-  factory :form do
-    name { "MyText" }
-    submission_email { "MyText" }
-    org { "MyText" }
-    live_at { "2022-12-16 11:29:45" }
-    privacy_policy_url { "MyText" }
-    form_slug { "MyText" }
-    what_happens_next_text { "MyText" }
-    support_email { "MyText" }
-    support_phone { "MyText" }
-    support_url_text { "MyText" }
-    declaration_text { "MyText" }
+  factory :form, class: "Form" do
+    sequence(:name) { |n| "Form #{n}" }
+    sequence(:form_slug) { |n| "form-#{n}" }
+    submission_email { Faker::Internet.email(domain: "example.gov.uk") }
+    privacy_policy_url { Faker::Internet.url(host: "gov.uk") }
+    org { "test-org" }
+    live_at { nil }
+    support_email { nil }
+    support_phone { nil }
+    support_url { nil }
+    support_url_text { nil }
+    what_happens_next_text { nil }
+    declaration_text { nil }
     question_section_completed { false }
     declaration_section_completed { false }
-    page_order { 1 }
+    page_order { nil }
+
+    trait :new_form do
+      submission_email { "" }
+      privacy_policy_url { "" }
+      pages { [] }
+    end
+
+    trait :ready_for_live do
+      support_email { Faker::Internet.email(domain: "example.gov.uk") }
+      what_happens_next_text { "We usually respond to applications within 10 working days." }
+      question_section_completed { true }
+      declaration_section_completed { true }
+    end
+
+    trait :live do
+      live_at { Time.zone.now }
+      support_email { Faker::Internet.email(domain: "example.gov.uk") }
+      what_happens_next_text { "We usually respond to applications within 10 working days." }
+      question_section_completed { true }
+      declaration_section_completed { true }
+    end
+
+
+    trait :with_support do
+      support_email { Faker::Internet.email(domain: "example.gov.uk") }
+      support_phone { Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4) }
+      support_url { Faker::Internet.url(host: "gov.uk") }
+      support_url_text { Faker::Lorem.sentence(word_count: 1, random_words_to_add: 4) }
+    end
   end
 end

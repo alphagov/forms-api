4702f4c979f8ad7a4ed9bcb54a1ded9b7c743185
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-12-16 12:51:40 +0000

    Add Form index and tests
    Co-authored-by: Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  scope "api/v1" do
+    resources :forms, controller: "api/v1/forms"
+  end
 end

a08c65cb4c788529592c156e37d98c24a0d9500e
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-12-20 13:57:23 +0000

    Add authenticate feature to check all requests
    Each request should have a token in the request header
    which we can compare to see if the request is allowed
    to get access to the api data in the response.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::API
   before_action :set_content_type
+  before_action :authenticate_request
 
   def set_content_type
     response.headers["Content-Type"] = "application/json"
+  end
+
+  def authenticate_request
+    unless request.headers["X-Api-Token"] == Settings.forms_api.authentication_key
+      render json: { status: "unauthorised" }, status: :unauthorized
+    end
   end
 end

4ff5ddc351ff8a4f217c858449eebd02cd0addff
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-12-23 14:23:48 +0000

    Fix failing blank param test
    Change error messages in forms when no parameters are passed in a post
    request.

--- app/controllers/api/v1/forms_controller.rb
+++ app/controllers/api/v1/forms_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::FormsController < ApplicationController
-  rescue_from ActionController::ParameterMissing do |_exception|
-    # We need to decide if we use the grape error messages, or new ones?
-    # render json: { error: exception.message }, status: :bad_request
-    render json: [{ messages: ["is missing"], params: %w[org] }], status: :bad_request
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: { error: exception.message }, status: :bad_request
   end
 
   def index

4bbd4b30ba97a8c06fdf8ded31dc0d8ffd383813
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-12-28 13:56:24 +0000

    Add sentry exception logging
    This change adds new environment variables which need to be set to use
    sentry - see the readme.
    
    The PaaS manifest will need to be changed before deployment.
    
    Co-authored-by: Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,9 @@
+if Settings.sentry.dsn.present?
+  Sentry.init do |config|
+    config.dsn = Settings.sentry.dsn
+    config.breadcrumbs_logger = %i[active_support_logger http_logger]
+    config.debug = true
+    config.traces_sample_rate = 1.0
+    config.environment = Settings.sentry.environment || "local"
+  end
+end

45ed0d3a62aaec64a2d4d1b8e55af0dfbfe9571d
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-01-05 11:02:44 +0000

    Delete sequel migrations and db config
    Co-authored-by: Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>

--- ruby-grape-version/db/migrations/012_add_what_happens_next_to_forms.rb
+++
@@ -1,8 +0,0 @@
-Sequel.migration do
-  up do
-    add_column :forms, :what_happens_next_text, String
-  end
-  down do
-    drop_column :forms, :what_happens_next_text
-  end
-end
--- ruby-grape-version/db/migrations/018_add_answer_options_to_page.rb
+++
@@ -1,8 +0,0 @@
-Sequel.migration do
-  up do
-    add_column :pages, :answer_settings, :jsonb
-  end
-  down do
-    drop_column :pages, :answer_settings
-  end
-end

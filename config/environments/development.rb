ea4e92c5f4809c7996a0f9aed0040ed642b1b49c
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-01-05 09:22:57 +0000

    Add .host.docker.internal to development
    When running forms admin and forms runner inside docker containers but
    the API from within docker, the API rejects calls from docker due to
    host authorization protection.
    
    See https://guides.rubyonrails.org/configuring.html#actiondispatch-hostauthorization
    
    This commit adds the docker internal hostname to list of trusted
    hostnames.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Add docker hostname for connecting from docker containers to host
+  # so we can run other components in docker and the API locally
+  config.hosts << ".host.docker.internal"
 end

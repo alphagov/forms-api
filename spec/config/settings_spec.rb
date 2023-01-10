62e4e33cd05ae177dc023e4fcf9b15c8921a289a
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-01-10 10:01:41 +0000

    Tweak Sentry config and add tests
    Changes to match what we have in forms-admin

---
+++ spec/config/settings_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+describe "Settings" do
+  settings = YAML.load_file(Rails.root.join("config/settings.yml")).with_indifferent_access
+  expected_value_test = "expected_value_test"
+
+  shared_examples expected_value_test do |key, source, expected_value|
+    describe ".#{key}" do
+      subject do
+        source[key]
+      end
+
+      it "#{key} has a default value" do
+        expect(subject).to eq(expected_value)
+      end
+    end
+  end
+
+  describe "forms api settings" do
+    forms_api = settings[:forms_api]
+
+    include_examples expected_value_test, :authentication_key, forms_api, "changeme"
+  end
+
+  describe "sentry" do
+    sentry = settings[:sentry]
+
+    include_examples expected_value_test, :dsn, sentry, nil
+
+    include_examples expected_value_test, :environment, sentry, "local"
+  end
+end

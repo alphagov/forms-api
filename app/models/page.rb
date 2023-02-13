da7fec4d73aee3a665b9674bdb4e5b0c47c4f5dc
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-02-13 11:30:58 +0000

    Remove legacy text answer types

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,7 @@ class Page < ApplicationRecord
   belongs_to :form
   acts_as_list scope: :form
 
-  ANSWER_TYPES = %w[single_line number address date email national_insurance_number phone_number long_text selection organisation_name text name].freeze
+  ANSWER_TYPES = %w[number address date email national_insurance_number phone_number selection organisation_name text name].freeze
 
   validates :question_text, presence: true
   validates :answer_type, presence: true, inclusion: { in: ANSWER_TYPES }

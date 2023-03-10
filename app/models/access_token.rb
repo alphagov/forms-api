ce0bd590d9730e460a55efb0d52fc024af91d160
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-03-10 09:55:04 +0000

    Add a scope to return only active tokens

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,3 +1,5 @@
 class AccessToken < ApplicationRecord
   validates :token, :owner, presence: true
+
+  scope :active, -> { where(deactivated_at: nil) }
 end
--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -19,4 +19,24 @@ RSpec.describe AccessToken, type: :model do
       expect(access_token.errors[:owner]).to include("can't be blank")
     end
   end
+
+  describe "scopes" do
+    describe "#active" do
+      let(:active_tokens) { create_list :access_token, 3 }
+      let(:deactivated_tokens) { create_list :access_token, 3, deactivated_at: Time.zone.now }
+
+      before do
+        active_tokens
+        deactivated_tokens
+      end
+
+      it "returns active tokens" do
+        expect(described_class.active).to eq(active_tokens)
+      end
+
+      it "does not return deactivated tokens" do
+        expect(described_class.active).not_to eq(deactivated_tokens)
+      end
+    end
+  end
 end

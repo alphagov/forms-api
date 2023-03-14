311153989a7a0efd64048956a5a4cf32d12b2fab
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-03-14 19:37:25 +0000

    Refactored access_token.rb model
    We decided that instead of using bcrypt to hash our tokens we would
    instead use sha-256. Although sha-256 is not a bulletproof as bcrypt it
    allows use to regenerate the hash without having to use a salt. bcrypt
    by default uses a dynamic generated salt and so when it hashes the token
    everytime it would use a different salt. This makes it difficult to search
    the table for a particular record without iterating through each record,
    using bcrypt to compare the hashed db value with the users token.

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,5 +1,19 @@
 class AccessToken < ApplicationRecord
   validates :token, :owner, presence: true
 
+  attr_accessor :users_token
+
   scope :active, -> { where(deactivated_at: nil) }
+
+  before_validation :generate_users_token, :set_token
+
+private
+
+  def generate_users_token
+    self.users_token = SecureRandom.uuid if users_token.blank?
+  end
+
+  def set_token
+    self.token = Digest::SHA2.new(256).hexdigest(users_token)
+  end
 end
--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -3,17 +3,26 @@ require "rails_helper"
 RSpec.describe AccessToken, type: :model do
   subject(:access_token) { described_class.new }
 
+  let(:new_access_token) { described_class.create!(owner: "joe_bloggs") }
+
+  before do
+    allow(SecureRandom).to receive(:uuid).and_return("testing-123")
+  end
+
   it "has a valid factory" do
-    access_token = create :access_token
-    expect(access_token).to be_valid
+    results = create :access_token
+    expect(results).to be_valid
+  end
+
+  it "generates a user token before validation" do
+    expect(new_access_token.users_token).to eq("testing-123")
+  end
+
+  it "generates a sha-256 token before validation" do
+    expect(new_access_token.token).to eq("8d9754db9759ab1785644440dbf19f88ab45ae326e421da6c1cb6e45140d534f")
   end
 
   describe "validations" do
-    it "requires a token" do
-      expect(access_token).to be_invalid
-      expect(access_token.errors[:token]).to include("can't be blank")
-    end
-
     it "requires an owner" do
       expect(access_token).to be_invalid
       expect(access_token.errors[:owner]).to include("can't be blank")
@@ -34,7 +43,7 @@ RSpec.describe AccessToken, type: :model do
         expect(described_class.active).to eq(active_tokens)
       end
 
-      it "does not return deactivated tokens" do
+      it "does not include deactivated tokens" do
         expect(described_class.active).not_to eq(deactivated_tokens)
       end
     end

alphagov/forms-api

commit 38b3a2cc940768a0f35cb30640ba6749599e9de1

Remove live_at column from forms table
We can now get the same information from the made_live_forms table.

app/models/form.rb
@@ -11,13 +11,17 @@ class Form < ApplicationRecord
 
   def make_live!(live_at = nil)
     live_at ||= Time.zone.now
-    update!(live_at: live_at, updated_at: live_at)
+    touch(time: live_at)
 
     made_live_forms.create!(json_form_blob: snapshot.to_json, created_at: live_at)
   end
 
   def draft_version
     snapshot.to_json
+  end
+
+  def live_at
+    return made_live_forms.last.created_at if made_live_forms.present?
   end
 
   def live_version
@@ -32,7 +36,7 @@ class Form < ApplicationRecord
   end
 
   def as_json(options = {})
-    options[:methods] ||= [:start_page]
+    options[:methods] ||= %i[live_at start_page]
     super(options)
   end
 

spec/models/form_spec.rb
@@ -78,7 +78,7 @@ RSpec.describe Form, type: :model do
   end
 
   describe "#make_live!" do
-    let(:form_to_be_made_live) { build :form }
+    let(:form_to_be_made_live) { create :form }
     let(:time_now) { Time.zone.now }
 
     before do
@@ -103,6 +103,18 @@ RSpec.describe Form, type: :model do
 
       expect(form.live_at).to eq(made_live_form.created_at)
       expect(form.updated_at).to eq(made_live_form.created_at)
+    end
+  end
+
+  describe "#live_at" do
+    it "returns nil if form has not been made live" do
+      form = create :form
+      expect(form.live_at).to be_nil
+    end
+
+    it "returns the time when the form was made live" do
+      made_live_form = create :made_live_form
+      expect(made_live_form.form.live_at).to eq made_live_form.created_at
     end
   end
 
@@ -132,7 +144,6 @@ RSpec.describe Form, type: :model do
         "submission_email",
         "org",
         "created_at",
-        "live_at",
         "updated_at",
         "privacy_policy_url",
         "form_slug",

db/migrate/20230324120801_remove_live_at_from_forms.rb
@@ -1,0 +1,15 @@
+class RemoveLiveAtFromForms < ActiveRecord::Migration[7.0]
+  def up
+    remove_column :forms, :live_at, :datetime
+  end
+
+  def down
+    add_column :forms, :live_at, :datetime
+
+    Form.find_each do |form|
+      if form.made_live_forms.present?
+        form.update!(live_at: form.made_live_forms.last.created_at)
+      end
+    end
+  end
+end

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_24_113043) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_24_120801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,7 +28,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_24_113043) do
     t.text "name"
     t.text "submission_email"
     t.text "org"
-    t.datetime "live_at"
     t.text "privacy_policy_url"
     t.text "form_slug"
     t.text "what_happens_next_text"

spec/factories/forms.rb
@@ -5,7 +5,6 @@ FactoryBot.define do
     submission_email { Faker::Internet.email(domain: "example.gov.uk") }
     privacy_policy_url { Faker::Internet.url(host: "gov.uk") }
     org { "test-org" }
-    live_at { nil }
     support_email { nil }
     support_phone { nil }
     support_url { nil }
@@ -51,8 +50,6 @@ FactoryBot.define do
 
     trait :live do
       ready_for_live
-      # TODO: remove live_at
-      live_at { Time.zone.now }
     end
 
     trait :with_support do

commit 49d6ec8d54f66575906c5b1118fe247d877df732

Fix form-runner
Commit 38b3a2c broke the forms-runner for newly made live forms, as form
blob did not include the live_at attribute required for validation of
the form [[1]], [[2]]. This manifested as `invalid live_at times` [[3]].

For backwards-compatibilty, this commit adds the live_at datetime back
into the form blob for made live forms. For draft form versions, live_at
is not needed, so we leave it out.

[1]: https://github.com/alphagov/forms-runner/blob/main/app/models/form.rb#L25-L30
[2]: https://github.com/alphagov/forms-runner/blob/31a0d62dab8a950d0df59e9a652c5006d9362d63/app/controllers/forms/base_controller.rb#L66
[3]: https://govuk-forms.sentry.io/issues/4034044478/

app/models/form.rb
@@ -13,7 +13,8 @@ class Form < ApplicationRecord
     live_at ||= Time.zone.now
     touch(time: live_at)
 
-    made_live_forms.create!(json_form_blob: snapshot.to_json, created_at: live_at)
+    form_blob = snapshot(live_at:)
+    made_live_forms.create!(json_form_blob: form_blob.to_json, created_at: live_at)
   end
 
   def has_draft_version
@@ -50,9 +51,9 @@ class Form < ApplicationRecord
     super(options)
   end
 
-  def snapshot
+  def snapshot(**kwargs)
     # override methods so it doesn't include things we don't want
-    as_json(include: [:pages], methods: [:start_page])
+    as_json(include: [:pages], methods: [:start_page]).merge(kwargs)
   end
 
   # form_slug is always set based on name. This is here to allow Form

spec/models/form_spec.rb
@@ -92,8 +92,18 @@ RSpec.describe Form, type: :model do
       expect(form_to_be_made_live.live_at).to eq(time_now)
     end
 
-    it "does create a made live version" do
-      expect(form_to_be_made_live.made_live_forms.last.json_form_blob).to eq form_to_be_made_live.snapshot.to_json
+    it "creates a made live version" do
+      expect(form_to_be_made_live.made_live_forms.last.json_form_blob)
+        .to eq(form_to_be_made_live.snapshot(live_at: time_now).to_json)
+    end
+
+    it "the made live version has a live_at datetime" do
+      form_blob = JSON.parse(
+        form_to_be_made_live.made_live_forms.last.json_form_blob,
+        symbolize_names: true,
+      )
+
+      expect(Time.zone.parse(form_blob[:live_at])).to eq time_now
     end
 
     it "makes timestamps consistent" do
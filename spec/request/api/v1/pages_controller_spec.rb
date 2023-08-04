46f17f6879fd0393d61a30c5a6629e58c6bd9373
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-08-04 12:41:03 +0100

    Rename pages detailed_guidance_markdown to match name used in forms-admin

--- spec/request/api/v1/pages_controller_spec.rb
+++ spec/request/api/v1/pages_controller_spec.rb
@@ -73,7 +73,7 @@ describe Api::V1::PagesController, type: :request do
                                                            created_at: "2023-01-01T12:00:00.000Z",
                                                            updated_at: "2023-01-01T12:00:00.000Z",
                                                            page_heading: nil,
-                                                           detailed_guidance_markdown: nil).as_json)
+                                                           additional_guidance_markdown: nil).as_json)
     end
 
     context "with params missing required keys" do

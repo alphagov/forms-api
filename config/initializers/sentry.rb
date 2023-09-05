9a5ee09a91c54701172966176c96ad3b65abbd31
Catalina Garcia <catalina.garcia@digital.cabinet-office.gov.uk>
2023-09-05 10:38:49 +0100

    TMP

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -7,3 +7,14 @@ if Settings.sentry.dsn.present?
     config.environment = Settings.sentry.environment
   end
 end
+
+# Uncomment out the below to test Sentry - this
+# will raise 2 issues in Sentry
+
+begin
+  1 / 0
+rescue ZeroDivisionError => exception
+  Sentry.capture_exception(exception)
+end
+
+Sentry.capture_message("test message")

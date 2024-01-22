b28c1960518e9994abfcafdc0a2103346b558912
Alistair Laing <aliuk2012@users.noreply.github.com>
2024-01-22 09:49:46 +0000

    stub out more states that we need for forms

--- app/state_machines/form_state_machine.rb
+++ app/state_machines/form_state_machine.rb
@@ -7,19 +7,39 @@ module FormStateMachine
     enum :state, {
       draft: "draft",
       live: "live",
+      draft_live: "draft_live",
+      archived: "archived",
+      draft_archived: "draft_archived"
     }
 
     aasm column: :state, enum: true do
       state :draft, initial: true
       state :live
+      state :draft_live
+      state :archived
+      state :draft_archived
 
       event :make_form_live do
         transitions from: :draft, to: :live, guard: Proc.new { task_status_service.mandatory_tasks_completed? }
       end
 
-      event :make_changes_live do
-        transitions from: :live, to: :draft, guard: Proc.new { has_live_version }
+      event :draft_new_live_form do
+        transitions from: :live, to: :draft_live, guard: Proc.new { has_live_version }
       end
+
+      event :make_draft_changes_live do
+        transitions from :draft_live, to: :live
+      end
+
+      event :archive_live_form do
+        # TODO: Is this only if a form is live and no draft or could a live form be archived even if there is an existing draft?
+        transitions from :live, to: :archived
+      end
+
+      event :create_draft_from_arhived_form do
+        transitions from: :archived, to: :draft_archived
+      end
+
     end
 
     private def task_status_service

1c622ad454fa49df7014f88812f7a57d3f1d6339
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2024-07-24 13:22:52 +0100

    Add answer type data to forms report
    For this part of the features report we query all forms and find out how
    many forms use each answer type.

--- app/controllers/api/v1/reports_controller.rb
+++ app/controllers/api/v1/reports_controller.rb
@@ -1,5 +1,28 @@
 class Api::V1::ReportsController < ApplicationController
   def features
-    render json: [].to_json, status: :ok
+    feature_stats = {
+      total_forms: forms.count,
+      answer_types: { name: number_of_forms_with_answer_type("name"),
+                      organisation_name: number_of_forms_with_answer_type("organisation_name"),
+                      phone_number: number_of_forms_with_answer_type("phone_number"),
+                      email: number_of_forms_with_answer_type("email"),
+                      address: number_of_forms_with_answer_type("address"),
+                      national_insurance_number: number_of_forms_with_answer_type("national_insurance_number"),
+                      date: number_of_forms_with_answer_type("date"),
+                      number: number_of_forms_with_answer_type("number"),
+                      selection: number_of_forms_with_answer_type("selection"),
+                      text: number_of_forms_with_answer_type("text") },
+    }
+    render json: feature_stats.to_json, status: :ok
+  end
+
+private
+
+  def forms
+    @forms ||= Form.all
+  end
+
+  def number_of_forms_with_answer_type(answer_type)
+    forms.filter { |form| form.pages.any? { |page| page.answer_type == answer_type } }.count
   end
 end
--- spec/requests/api/v1/reports_controller_spec.rb
+++ spec/requests/api/v1/reports_controller_spec.rb
@@ -2,8 +2,47 @@ require "rails_helper"
 
 RSpec.describe Api::V1::ReportsController, type: :request do
   describe "GET /features" do
-    it "returns http success" do
+    before do
+      create :form, pages: [
+        (build :page, answer_type: "text"),
+        (build :page, answer_type: "text"),
+      ]
+
+      create :form, pages: [
+        (build :page, answer_type: "name"),
+        (build :page, answer_type: "organisation_name"),
+        (build :page, answer_type: "phone_number"),
+        (build :page, answer_type: "email"),
+        (build :page, answer_type: "address"),
+        (build :page, answer_type: "national_insurance_number"),
+        (build :page, answer_type: "date"),
+        (build :page, answer_type: "number"),
+        (build :page, answer_type: "selection"),
+        (build :page, answer_type: "text"),
+      ]
+
       get "/api/v1/reports/features"
+    end
+
+    it "returns the breakdown of form features used" do
+      expect(response.body).to eq({
+        total_forms: 2,
+        answer_types: {
+          name: 1,
+          organisation_name: 1,
+          phone_number: 1,
+          email: 1,
+          address: 1,
+          national_insurance_number: 1,
+          date: 1,
+          number: 1,
+          selection: 1,
+          text: 2,
+        },
+      }.to_json)
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
     end
   end

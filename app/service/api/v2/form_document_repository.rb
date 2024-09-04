9407a98400602e8fad22a0e2eef1918b642f81f1
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-09-04 09:19:33 +0300

    Add service object for getting v2 API form documents
    We want to be able to get v2 form documents for v1 forms, and in future
    get v2 form documents from a table.
    
    It would be nice if there was one unified interface for doing this. I
    think it makes sense to use something like the repository pattern, where
    we have a class that gets the form document from the right place.
    
    Putting this in a service object allows us to keep the logic out of our
    models.
    
    I've tried to make the methods similar to methods that might be on a
    model so in a future where we only have one way of getting form
    documents we can replace the service object with a normal simple model.

---
+++ app/service/api/v2/form_document_repository.rb
@@ -0,0 +1,29 @@
+class Api::V2::FormDocumentRepository
+  class << self
+    def find(form_id, tag)
+      v1_form = Form.find(form_id)
+      v1_blob = v1_blob(v1_form, tag)
+      raise ActiveRecord::RecordNotFound, "Could not find #{tag} form document for #{v1_form.inspect}" if v1_blob.blank?
+
+      form_snapshot = JSON.parse!(v1_blob)
+      converter.to_api_v2_form_document(form_snapshot)
+    end
+
+  private
+
+    def converter
+      @converter ||= Api::V2::Converter.new
+    end
+
+    def v1_blob(v1_form, tag)
+      case tag.to_sym
+      when :draft
+        v1_form.draft_version if v1_form.has_draft_version
+      when :live
+        v1_form.live_version if v1_form.has_live_version
+      when :archived
+        v1_form.archived_live_version if v1_form.has_been_archived
+      end
+    end
+  end
+end
---
+++ spec/service/api/v2/form_document_repository_spec.rb
@@ -0,0 +1,133 @@
+require "rails_helper"
+
+RSpec.describe Api::V2::FormDocumentRepository do
+  describe ".find" do
+    it "finds a form document given form id and document tag" do
+      form = create :form
+      expect(described_class.find(form.id, :draft)).to be_truthy
+    end
+
+    it "returns a JSON object" do
+      form = create :form
+      form_document = described_class.find(form.id, :draft)
+      expect(form_document.as_json).to eq form_document
+    end
+
+    it "raises an exception if the form does not exist" do
+      expect {
+        described_class.find("not_a_form", :draft)
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it "raises an exception if the form does not have a document for the given tag" do
+      form = create :form
+      expect {
+        described_class.find(form.id, :live)
+      }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    context "when there is a v1 form for the given form id" do
+      let(:form) { create :form }
+
+      it "returns a v2 form document" do
+        form = create :form, :with_pages
+        expect(described_class.find(form.id, :draft))
+          .to include("steps")
+      end
+
+      it "uses the current form snapshot if the draft tag is given" do
+        form.update! name: "Edited form"
+        expect(described_class.find(form.id, :draft))
+          .to include("name" => "Edited form")
+      end
+
+      context "when the form has been made live" do
+        let(:form) { create :form, :live, name: "Test form" }
+
+        it "uses the made live form snapshot if the live tag is given" do
+          expect(described_class.find(form.id, :live))
+            .to include("name" => "Test form")
+        end
+
+        it "raises an exception if the draft tag is given" do
+          expect {
+            described_class.find(form.id, :draft)
+          }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
+        it "raises an exception if the archived tag is given" do
+          expect {
+            described_class.find(form.id, :archived)
+          }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
+        context "and then edited" do
+          before do
+            form.update! name: "Edited form"
+            form.create_draft_from_live_form!
+          end
+
+          it "uses the made live form snapshot if the live tag is given" do
+            expect(described_class.find(form.id, :live))
+              .to include("name" => "Test form")
+          end
+
+          it "uses the current form snapshot if the draft tag is given" do
+            expect(described_class.find(form.id, :draft))
+              .to include("name" => "Edited form")
+          end
+
+          it "raises an exception if the archived tag is given" do
+            expect {
+              described_class.find(form.id, :archived)
+            }.to raise_error(ActiveRecord::RecordNotFound)
+          end
+        end
+      end
+
+      context "when the form has been archived" do
+        let(:form) { create :form, :archived, name: "Test form" }
+
+        it "uses the made live form snapshot if the archived tag is given" do
+          expect(described_class.find(form.id, :archived))
+            .to include("name" => "Test form")
+        end
+
+        it "raises an exception if the draft tag is given" do
+          expect {
+            described_class.find(form.id, :draft)
+          }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
+        it "raises an exception if the live tag is given" do
+          expect {
+            described_class.find(form.id, :live)
+          }.to raise_error(ActiveRecord::RecordNotFound)
+        end
+
+        context "and then edited" do
+          before do
+            form.update! name: "Edited form"
+            form.create_draft_from_archived_form!
+          end
+
+          it "uses the made live form snapshot if the archived tag is given" do
+            expect(described_class.find(form.id, :archived))
+              .to include("name" => "Test form")
+          end
+
+          it "uses the current form snapshot if the draft tag is given" do
+            expect(described_class.find(form.id, :draft))
+              .to include("name" => "Edited form")
+          end
+
+          it "raises an exception if the live tag is given" do
+            expect {
+              described_class.find(form.id, :live)
+            }.to raise_error(ActiveRecord::RecordNotFound)
+          end
+        end
+      end
+    end
+  end
+end
